d2283bcb8882682051d45a294cd2a41c3fc3edd5
ajay-joshi27 <ajayajoshi143@gmail.com>
2025-05-01 15:27:23 +0530

    StringEquals

---
+++ src/StringEquals.java
@@ -0,0 +1,10 @@
+public class StringEquals {
+public static void main(String[] args) {
+    String a="java";
+    if (a.equals("java")) {
+        System.out.println(" MATCHING CODE ");
+    } else {
+        System.out.println("not matching");
+    }
+}
+}
---
+++ src/multipleofthreeorseven.java
@@ -0,0 +1,14 @@
+public class multipleofthreeorseven {
+    public static void main(String[] args) {
+        
+    
+    int num = 21;
+
+    if (num % 3 == 0 || num % 7 == 0) {
+        System.out.println(num + " is a multiple of 3 or 7");
+    } else {
+        System.out.println(num + " is not a multiple of 3 or 7");
+    }
+    
+}
+}
